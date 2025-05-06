8d969a629b6ac3c15db82c2c93f94d4fe593c732
Rachel Yorke <116943009+rachelyr@users.noreply.github.com>
2025-05-06 19:05:46 +0530

    implemented authorization
    authorization implemented using authtoken

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 from rest_framework import generics
 from .models import Menu, BookingTable
@@ -10,14 +10,16 @@ from .serializers import MenuSerializer, BookingTableSerializer
 
 # Create your views here.
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(generics.RetrieveAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingView(ModelViewSet):
     queryset = BookingTable.objects.all()
     serializer_class = BookingTableSerializer
-    #permission_classes = [permissions.isAuthenticated]
+    permission_classes = [IsAuthenticated]
