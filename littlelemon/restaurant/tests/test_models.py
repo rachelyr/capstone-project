40a33c3690f6b853a8ab361e9a7f3199b29fac8b
Rachel Yorke <116943009+rachelyr@users.noreply.github.com>
2025-05-11 18:54:00 +0530

    Test written

--- littlelemon/restaurant/tests/test_models.py
+++ littlelemon/restaurant/tests/test_models.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
-from restaurant.models import Menu
+from restaurant.models import Menu, BookingTable
 
 class MenuItemTest(TestCase):
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price = 80, inventory = 100)
         self.assertEqual(str(item), "IceCream : 80")
+
+    def test_create_item(self):
+        menuItem = Menu.objects.create(
+            title="Pancakes",
+            price= 240,
+            inventory= 40
+        )
+        self.assertEqual(str(menuItem), "Pancakes : 240")
